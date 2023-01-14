841c7d74f7e72846b9c8630213a7e871ef3ea03a
TalisTalis <tsarst@gmail.com>
2023-01-14 12:19:13 +0300

    sv

--- CodeBlog.Array/Program.cs
+++ CodeBlog.Array/Program.cs
@@ -8,8 +8,41 @@ namespace CodeBlog.Array
 {
     internal class Program
     {
+        enum Stages
+            {
+                analysis,
+                repair,
+                performance,
+                disassembly,
+                revision = 2,
+                setting,
+                assembly,
+                firstinclusion
+            }
         static void Main(string[] args)
         {
+            int[] arr = { 43, 12, 7, 61, 0 };
+
+            List<int> list = new List<int>()
+            { 
+                43,
+                12,
+                7,
+                61,
+                0
+            };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i} элемент массива arr[] = {arr[i]}");
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i} элемент списка list = {list[i]}");
+            }
+
+            Console.WriteLine($"Прибор на этапе {Stages.firstinclusion}");
         }
     }
 }
